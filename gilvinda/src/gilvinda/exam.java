3113f19f71787cf55da4c03c290ef259df5f6c49
pc <pc@192.168.1.35>
2024-02-09 22:20:58 -0800

    commit one

---
+++ gilvinda/src/gilvinda/exam.java
@@ -0,0 +1,14 @@
+package gilvinda;
+
+public class exam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1=10,num2=20,c;
+		c=num1+num2;
+		System.out.println("the sum is "+c);
+		
+	}
+
+}
