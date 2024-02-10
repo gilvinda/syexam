135d31924e4db07a1960ad3d0ea5a28d2fc4330e
pc <pc@192.168.1.35>
2024-02-09 22:25:06 -0800

    second commit

--- gilvinda/src/gilvinda/exam.java
+++ gilvinda/src/gilvinda/exam.java
@@ -5,10 +5,12 @@ public class exam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int num1=10,num2=20,c;
+		int num1=10,num2=20,c,d;
 		c=num1+num2;
 		System.out.println("the sum is "+c);
 		
+		d=num1-num2;
+		System.out.println("the sub is "+d);
 	}
 
 }
